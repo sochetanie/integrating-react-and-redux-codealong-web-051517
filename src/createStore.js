e9922bc852a7467d7a958bdaf1cbdbdc8598d6cd
sochetanie <allagorik2901@gmail.com>
2017-08-03 10:48:41 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,1 +1,20 @@
+import {render} from './index'
 
+export default function createStore(reducer) {
+  let state
+
+  function dispatch(action) {
+    state = reducer(state, action)
+    render()
+  }
+
+  function getState() {
+    return state
+  }
+
+  return {
+    dispatch,
+    getState
+  }
+
+}
